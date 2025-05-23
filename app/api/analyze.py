51a9661f34aeae3ee05d76e1363c6de98481d2b9
mete0rfish <sungwon326@naver.com>
2025-05-23 14:45:23 +0900

    feat: 시간 측정 추가

--- app/api/analyze.py
+++ app/api/analyze.py
@@ -8,6 +8,8 @@ import app.api.s3_manager as s3_manager
 import ai.app as ai_model
 import ai.graphology as personality_ai_model
 
+import time
+
 router = APIRouter()
 
 @router.get("/test")
@@ -22,9 +24,12 @@ def generate(request: Request, req_body: AnalyzeRequest):
     s3_manager.download_reference_images(req_body.comparisonImageUrls)
     s3_manager.download_test_image(req_body.verificationImageUrl)
 
+    start = time.time();
     # 2. 감정 시작
     appraisal_response = ai_model.analyze(model)
     appraisal_response.verificationImageUrl = req_body.verificationImageUrl
+
+    print('latency: ', time.time() - start)
 
     # 3. 이미지 삭제
     s3_manager.delete_reference_images()
